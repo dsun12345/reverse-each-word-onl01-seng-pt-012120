ee2bdc37e19bd1dc862e7233102244bc4f01dfec
Daniel Sun <danielsun12345@gmail.com>
2020-01-29 10:55:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ def reverse_each_word (x)
   original_array = x.split(" ")
   return_array = []
   original_array.each {|string| return_array << string.reverse}
-  return_array
+  return_array.join
 end
