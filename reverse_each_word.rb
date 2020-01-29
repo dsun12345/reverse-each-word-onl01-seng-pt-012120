e1d5e63b0b0cd7f6d20877baf5d0486cb664ddcb
Daniel Sun <danielsun12345@gmail.com>
2020-01-29 11:02:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,5 +8,5 @@ end
 def reverse_each_word (x)
   array = x.split(" ")
   test_array = []
-  array.collect {|string| test_array << string.reverse
+  array.collect {|string| test_array << string.reverse}
 end
